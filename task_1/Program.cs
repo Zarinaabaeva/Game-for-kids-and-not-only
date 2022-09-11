1ff10394ffcf4ce8f8940c7322ffeb9fe976ad0b
Zarina <abaevazarina@yandex.ru>
2022-09-12 00:31:11 +0300

    commit

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,25 +1,36 @@
-﻿Console.WriteLine("Введите число:");
+﻿m1:
+Console.WriteLine("Введите число от 1 до 10:");
 int a = Convert.ToInt32(Console.ReadLine());
+if(a < 1 || a > 10) goto m1;
 
-int sum = a;
-while(sum < 100)
-{
-  Console.WriteLine("Введите число: ");
-  int b = Convert.ToInt32(Console.ReadLine());
-  sum = sum + b;
-  Console.WriteLine($" сумма -> {sum}");
-   if(sum > 100)
-   {
+ int sum = a;
+
+    while(sum < 100)
+    {
+    Console.WriteLine("Введите число от 1 до 10: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    if( b > 1 && b < 10)
+    {
+      sum = sum + b;
+      Console.WriteLine($" сумма -> {sum}");
+    }
+    if(sum > 100)
+    {
       sum = sum - b;
       Console.WriteLine("Введите такое число, чтобы сумма с предыдущим числом не превышала 100: ");
       int c = Convert.ToInt32(Console.ReadLine());
       sum = sum + c;
-   }
+    }
    if(sum == 100)
    {
     Console.WriteLine("Вы выиграли!");
    }
-}
+    } 
+ 
+
+ 
+
+
 
 
 
