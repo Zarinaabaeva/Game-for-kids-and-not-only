51b11e8f18bd0c961bb57f7f711ed0abbdbf269d
Zarina <abaevazarina@yandex.ru>
2022-09-11 23:05:15 +0300

    added task_1 and Readme.md

---
+++ task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum = a;
+while(sum < 100)
+{
+  Console.WriteLine("Введите число: ");
+  int b = Convert.ToInt32(Console.ReadLine());
+  sum = sum + b;
+  Console.WriteLine($" сумма -> {sum}");
+   if(sum > 100)
+   {
+      sum = sum - b;
+      Console.WriteLine("Введите такое число, чтобы сумма с предыдущим числом не превышала 100: ");
+      int c = Convert.ToInt32(Console.ReadLine());
+      sum = sum + c;
+   }
+   if(sum == 100)
+   {
+    Console.WriteLine("Вы выиграли!");
+   }
+}
+
+
+
+
