d172e08f9957dd4956acf3c399be10ce9a645b9b
Zarina <abaevazarina@yandex.ru>
2022-09-20 02:55:40 +0300

    changed task

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,34 +1,36 @@
-﻿m1:
-Console.WriteLine("Введите число от 1 до 10:");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-if(firstNumber < 1 || firstNumber> 10) goto m1;
+﻿int firstNumber = 0;
+while (firstNumber < 1 || firstNumber > 10)
+{
+    Console.WriteLine("Введите число от 1 до 10:");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+}
 
- int sum = firstNumber;
+int sum = firstNumber;
 
-    while(sum < 100)
-    {
+while (sum < 100)
+{
     Console.WriteLine("Введите число от 1 до 10: ");
     int secondNumber = Convert.ToInt32(Console.ReadLine());
-    if( secondNumber > 1 && secondNumber < 10)
+    if (secondNumber > 1 && secondNumber < 10)
     {
-      sum = sum + secondNumber;
-      Console.WriteLine($" сумма -> {sum}");
+        sum = sum + secondNumber;
+        Console.WriteLine($" сумма -> {sum}");
     }
-    if(sum > 100)
+    if (sum > 100)
     {
-      sum = sum - secondNumber;
-      Console.WriteLine("Введите такое число, чтобы сумма с предыдущим числом не превышала 100: ");
-      int correctNumber = Convert.ToInt32(Console.ReadLine());
-      sum = sum + correctNumber;
+        sum = sum - secondNumber;
+        Console.WriteLine("Введите такое число, чтобы сумма с предыдущим числом не превышала 100: ");
+        int correctNumber = Convert.ToInt32(Console.ReadLine());
+        sum = sum + correctNumber;
     }
-   if(sum == 100)
-   {
-    Console.WriteLine("Вы выиграли!");
-   }
-    } 
- 
+    if (sum == 100)
+    {
+        Console.WriteLine("Вы выиграли!");
+    }
+}
 
- 
+
+
 
 
 
